6e8e18452df4056071fd5fe0a51771daf6850e92
Othman <othmanosx@gmail.com>
2024-04-22 03:03:58 +0300

    Feat: create menu section

---
+++ src/components/Menu.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa6';
+
+const CardComponent = ({ imageSrc, altText, title, description, price }) => (
+  <div className="card">
+    <div className="card__image">
+      <img src={imageSrc} alt={altText} />
+    </div>
+    <div className="card__info">
+      <div className="card__info--title">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <div>
+          {[...Array(5)].map((_, index) => (
+            <FaStar color="gold" />
+          ))}
+        </div>
+      </div>
+      <div className="card__info--price">
+        <p>{price}</p>
+      </div>
+    </div>
+  </div>
+);
+
+const Menu = () => {
+  const items = [
+    {
+      id: 1,
+      imageSrc:
+        'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      altText: 'Salad',
+      title: 'Salad',
+      description: 'Fresh & sweet',
+      price: '$ 5',
+    },
+    {
+      id: 2,
+      imageSrc:
+        'https://images.pexels.com/photos/840216/pexels-photo-840216.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      altText: 'Fish',
+      title: 'Fish',
+      description: 'Fresh & sweet',
+      price: '$ 5',
+    },
+    {
+      id: 3,
+      imageSrc:
+        'https://images.pexels.com/photos/4001871/pexels-photo-4001871.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      altText: 'Pizza',
+      title: 'Pizza',
+      description: 'Fresh & sweet',
+      price: '$ 5',
+    },
+  ];
+
+  return (
+    <section className="container" id="menu">
+      <h2 class="our-menu">Our Menu!</h2>
+      <div className="art-board__container">
+        {items.map((item) => (
+          <CardComponent
+            key={item.id}
+            imageSrc={item.imageSrc}
+            altText={item.altText}
+            title={item.title}
+            description={item.description}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Menu;
