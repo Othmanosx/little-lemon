2f6903d88ea368d54c58da2caef7a7f8f0ca7014
Othman <othmanosx@gmail.com>
2024-04-23 03:02:10 +0300

    Feat: add confirmation page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Main from './views/Main';
 import Layout from './views/Layout';
 import BookingForm from './views/BookingForm';
+import BookingConfirmation from './views/BookingForm/BookingConfirmation';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Main />} />
         <Route path="reservations" element={<BookingForm />} />
+        <Route path="booking-confirmation" element={<BookingConfirmation />} />
       </Route>
     </Routes>
   );
---
+++ src/views/BookingForm/BookingConfirmation.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const BookingConfirmation = () => {
+  const [localStorageData, setLocalStorageData] = useState(null);
+  const [submit, setSubmit] = useState(false);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('formData');
+
+    setLocalStorageData(storedData ? JSON.parse(storedData) : null);
+  }, []);
+  const handleBackButtonClick = () => {
+    // Remove the data from local storage
+    localStorage.removeItem('formData');
+    setSubmit(false);
+  };
+
+  return (
+    <section className="confirmation-container">
+      <div className="booking-form">
+        <div className="container">
+          <h2 className="text-center">Confirm the data</h2>
+          {localStorageData && (
+            <>
+              <div>
+                <strong>Name:</strong> {localStorageData.name}
+              </div>
+              <div>
+                <strong>Email:</strong> {localStorageData.email}
+              </div>
+              <div>
+                <strong>Phone:</strong> {localStorageData.phone}
+              </div>
+              <div>
+                <strong>Date:</strong> {localStorageData.date}
+              </div>
+              <div>
+                <strong>Time:</strong> {localStorageData.time}
+              </div>
+              <div>
+                <strong>Guests:</strong> {localStorageData.guests}
+              </div>
+              <div>
+                <strong>Occasion:</strong> {localStorageData.occasion}
+              </div>
+              <div>
+                <strong>Additional information:</strong>{' '}
+                {localStorageData.description}
+              </div>
+            </>
+          )}
+          <div className="text-center">
+            <Link
+              to="/reservations"
+              className="btn-edit"
+              onClick={handleBackButtonClick}
+            >
+              Edit
+            </Link>
+            <button
+              type="button"
+              onClick={() => setSubmit(true)}
+              className="btn-book"
+            >
+              Book Now
+            </button>
+          </div>
+          {submit && (
+            <p className="text-center">
+              <strong>Great! Reservation completed successfully.</strong>
+            </p>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BookingConfirmation;
